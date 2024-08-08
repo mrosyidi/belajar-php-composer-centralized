6bde5b7ef6c316a6ede4a11c2abe6719c30f874b
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-08-08 10:08:34 +0700

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '57cd66a5abe946576ba1e0a86a64db32ce31e4a9',
+        'name' => 'programmerzamannow/belajar-php-composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'programmerzamannow/belajar-php-composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '57cd66a5abe946576ba1e0a86a64db32ce31e4a9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
